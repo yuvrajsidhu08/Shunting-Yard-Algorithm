b6fa7c23c128e27dfde799541d2af26ea730a0b8
yuvrajsidhu08 <454009@bsd48.org>
2026-03-15 22:45:40 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -50,3 +50,41 @@ Node* dequeue(Node*& f, Node*& r){
     t->next=NULL;
     return t;
 }
+
+bool op(char* t){
+    return !strcmp(t,"+")||!strcmp(t,"-")||!strcmp(t,"*")||!strcmp(t,"/")||!strcmp(t,"^");
+}
+
+int prec(char* o){
+    if(!strcmp(o,"^")) return 3;
+    if(!strcmp(o,"*")||!strcmp(o,"/")) return 2;
+    if(!strcmp(o,"+")||!strcmp(o,"-")) return 1;
+    return 0;
+}
+
+void shunt(char in[], Node*& f, Node*& r){
+    Node* stack=NULL;
+    char* t=strtok(in," ");
+
+    while(t){
+        if(isdigit(t[0])) enqueue(f,r,new Node(t));
+
+        else if(op(t)){
+            while(stack && op(peek(stack)->val) && prec(peek(stack)->val)>=prec(t))
+                enqueue(f,r,pop(stack));
+            push(stack,new Node(t));
+        }
+
+        else if(!strcmp(t,"(")) push(stack,new Node(t));
+
+        else if(!strcmp(t,")")){
+            while(stack && strcmp(peek(stack)->val,"("))
+                enqueue(f,r,pop(stack));
+            pop(stack);
+        }
+
+        t=strtok(NULL," ");
+    }
+
+    while(stack) enqueue(f,r,pop(stack));
+}
